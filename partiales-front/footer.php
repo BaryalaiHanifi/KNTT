96f7127d4d0b79a7061e360fa5c1c6c799afec09
Baryalai Hanifi <148928145+BaryalaiHanifi@users.noreply.github.com>
2024-04-21 18:17:32 +0430

    footer responsive

--- partiales-front/footer.php
+++ partiales-front/footer.php
@@ -1,28 +1,33 @@
  <footer id="main-footer">
-   	<div class="container2">
-   		<div id="footer">
-    	<span>We acknowledge your participation in social media with 
+ 	<div class="conatainer-fluid">
+ 		<div class="row" id="footer">
+ 		<div class="col-md-6" >
+		<span>01</span>
+		<strong><a href="index.php">Home</a></strong>
+    	<hr>
+		<span>02</span>
+		<strong><a href="mission.php">Mission</a></strong>
+		<hr>
+		<span>03</span>
+    	<strong><a href="recipes.php">Recipes</a></strong>
+		<hr>
+		<span>04</span>
+    	<strong><a href="contact.php">Contact</a></strong>
+		<hr>	
+		</div>
+		<div class="col-md-6" style="display:grid; place-items:center;margin-bottom:50px;">
+		<p style="margin-left: 20px;">We acknowledge your participation in social media with 
     		Kabul Night Travel & Tours family, as well your
     		sugestion about our services and 
     		share your best travel experinces!!!
 			<br><br>
-			<a href="https://www.facebook.com/profile.php?id=61553712674280" style="float: right;"><img src="images/facebook.png"></a>
-    		<a href="https://www.instagram.com/kabulnights4?igsh=MWoyd2xmZGx5cjFvZQ==" style="float: right;"><img src="images/insta.png"></a>
-    		<a href="info@kabulnight.com" style="float: right;"><img src="images/email.JPG"></a>
-		</span>
+			<a href="https://www.facebook.com/profile.php?id=61553712674280"><img src="images/facebook.png"></a>
+    		<a href="https://www.instagram.com/kabulnights4?igsh=MWoyd2xmZGx5cjFvZQ=="><img src="images/insta.png"></a>
+    		<a href="info@kabulnight.com"><img src="images/mail.png"></a>
+		</p>
 		</div>
-   		<strong><a href="mission.php">Mission</a></strong>
-   		<span>01</span>
-    	<hr>
-    	<strong><a href="recipes.php">Recipes</a></strong>
-    	<span>02</span>
-    	<hr>
-    	<strong><a href="contact.php">Contact</a></strong>	
-    	<span>03</span>
-    	<hr>
-    	<div id="copyrigt">
-
-    	<p>&copy;
+		<div class="col-md-12" id="copyrigt" style="display:grid; place-items:center;margin-bottom:50px;">
+			<p>&copy;
         <?php
         $startYear = 2023;
         $currentYear = date('Y');
@@ -34,10 +39,8 @@
              echo "$startYear&ndash;$currentYear";
         } 
          ?>
-        Powered by:Baryalai Hanifi</p>
-        </div>
-        </div>
-    	
+        Developed by-Baryalai Hanifi</p>
+		</div>
     </footer>
 
 </body>
