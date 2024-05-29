e9c23014e2fe66224d26835cd0592e61caf51cca
Baryalai Hanifi <148928145+BaryalaiHanifi@users.noreply.github.com>
2024-05-29 17:07:48 +0430

    some small changes

--- index.php
+++ index.php
@@ -1,72 +1,70 @@
 <?php include('./partiales-front/menu.php');?>
 <?php include('./config/constants.php'); ?>
+<br>
 <header style="background-color: white;">
-<div class="container-fluid" style="background-color: white;">
+<div class="container-fluid"  id="background-image-container1">
 		<div class="row">
-			<div class="col-md-12" id="h1-sty" style="text-align: center;">
-				<h2>We Plan, You Enjoy!</h2>
+			<div class="col-md-12" id="h1-sty" style="text-align: center;
+	color:white;margin-bottom:200px;margin-top:200px;font-size:130px;">
+				<h1>We Plan, You Enjoy!</h1>
 				<img src="images/slogan.png" class="img-fluid">
 			</div>
 		</div>
-</div>	
+</div>
+
 <div class="container-fluid">
 	<div class="row">
+		<hr style="color: lightgreen;">
 			<img src="images/KnttServices.jpeg" class="img-fluid" style="width: 100%;">
+		<hr style="color: lightgreen;">
 	</div>
 </div>
-</header>
+</header><br><br>
 	
 <div class="container-fluid">
 		<div class="row">
 			<div class="col-md-6" id="showcase">
-				<img src="images/section-1.jpg" class="img-fluid">
-				<h1><small>02</small>The best travel Options for you!</h1>
-				<p>We have gathered best suppliers together for (travel packages ,visa & ticket) to top countries including Russia,Uman,Brazil,Iran,Pakistan,Dubai, etc.<br><br>
-			   	Learn more about our mission in below link.<br><br>
+				<img src="images/section-1.jpg" class="img-fluid" style="border-radius: 2px;">
+				<h1>The Best Travel Options For You!</h1>
+				<p style="text-align: justify;">Explore new adventures with us! Whether you want to visit exciting cities,
+					 relax on sunny beaches, or dive into local cultures, we've got the perfect trip for you. Let's make your travel dreams come true. Start planning today!<br><br>
            		<a href="mission.html">Discover Our mission</a>		    
 		   		</p>		
 			</div>
 			<div class="col-md-6" id="showcase">
-			<h1><small>01</small>The best travel packages for you!</h1>
-			<p>We have gathered best suppliers together for (travel packages ,visa & ticket) to top countries including Russia,Uman,Brazil,Iran,Pakistan,Dubai, etc.<br><br>
+			<h1>The Best Travel Packages For You!</h1>
+			<p style="text-align: justify;">We've brought together top-notch suppliers to create fantastic travel packages, complete with visas and tickets, for some of the most sought-after destinations around the globe.<br><br>
 		    Learn more about our mission in below link.<br><br>
-            <a href="mission.html">Discover Our mission</a>		    
+            <a href="mission.php">Discover Our mission</a>		    
 		    </p>		
-			<img src="images/section-1-2.jpg" class="img-fluid">
+			<img src="images/section-1-2.jpg" class="img-fluid" style="border-radius: 2px;">
 			</div>
 		</div>	
 	    <div >
 </div>
-<div class="container-fluid" id="img3">
-	<div class="row">
-		<div class="col-md-12"> 
-			<img src="images/section-1-3.jpg" class="img-fluid">
-		</div>
-	</div>
-</div>
-
+<br><br>
 <div class="container-fluid" id="main">
 	<div class="row">
 		<div class="col-md-12">
-		<h2><small>03 </small> Born To Be Curious.</h2>
+		<h2><small>-</small> Born To Be Curious.</h2>
     	
 		</div>
 	</div>
 	<div class="row">
 		<div class="col-md-6">
-		<p>
-				We have tried to explore best<br> destinations all around the globe
-				and we go on this process as well,<br> destinations that are famous for position, story, design<br> and architechture.
+		<p style="text-align: justify;margin-right:20px;">	Your gateway to endless exploration! We're here to fuel your wanderlust and inspire your curiosity. From breathtaking landscapes to vibrant cultures, embark on journeys that
+		 ignite your sense of adventure. Let's satisfy your curiosity, one destination at a time.
 				<br><br><br>
-				Game to try some of out destinations? Discover the ease & versatility of traveling with effortless process.
+				Ready to experience our destinations? Discover the convenience and flexibility of travel with our seamless process.
 				<br><br>
 				<a href="recipes.html">Discover Our Destinations</a>
-
 		</p>
 		</div>
+
 		<div class="col-md-6">
-		<img src="images/logo4.jpg"><h2>
+		<img src="images/logo4.jpg" style="margin-bottom: 40px;">
 		</div>
+
 	</div>
   </div>
   <br>
